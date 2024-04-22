be3c7ddbd8cf6f4e810c2fb6daaede1828e50e27
Scott Prive <scottprive@gmail.com>
2024-04-21 21:35:36 -0400

    fix bet logic and complete. LGTM.

--- README.md
+++ README.md
@@ -1,23 +1,111 @@
 
-## Acey Ducey
+# Acey Ducey
+[![License: ODbL](https://img.shields.io/badge/License-PDDL-brightgreen.svg)](https://opendatacommons.org/licenses/pddl/)
 
-A simple port of the Creative Computing game.
+## Description
+This is a Go port of "Acey Ducey", based on the 1978 edition of
+BASIC Computer Games (BGC) from Creative Computing.
+
 https://www.atariarchives.org/basicgames/showpage.php?page=2
 
+NOTE: The game is actually "Acey Deucey", and sometimes called "In-Between".
+"Acey Ducey" is simply the spelling used in BCG.
 
+## Purpose
+Just getting used to Go. When BASIC was a thing, this was the first code I ever typed in and debugged.
+So why not in Go? 
+
+## Design Process
+
+Flowchart the logic using [mermaid-js](https://github.com/mermaid-js/mermaid).
+
+1. Review actual card game rules (found discrepencies vs the ruleset hosted by Bicycle Cards)
+1. Review code in the book
+1. Review other implementations: rando Github, the `coding-horror` repo, and forums. 
+1. Flowchart original logic
+
+To study any of these BCG listings, it helps to annotate printed code, or flowchart it.
+The BASIC language variant used in the book did not support GOSUB, or even "THEN" and "ELSE".
+Their workaround was spaghetti GOTOs, sometimes statement cramming, which makes it harder to read.
+
+## Game Logic (Approximate)
+
+This version follows the original book listing's logic and rules. Note that the book deviates
+from more traditional card rules, as found on the Bicycle Cards website or Wikipedia.
 
 ```mermaid
 flowchart TD
     
     A[Introduction] --> B[Your money remaining]
-    B --> C[Draw card]
-    C --> D[Display cards]
-    D --> E[Get bet]
-    E --> F[Draw card - final]
-    F --> Y[Try Again?]
+    B --> E[Draw dealer cards]
+    // Evaluate dealer cards
+    E --> F[Display cards]
+    F --> G[Get bet]
+    G --> H[Draw player card]
+    H --> I [Evaluate player card]
+    H --> Y[Try Again?]
     Y --> |Yes| B
     Y --> |No| Z
     Z[END]
-
-
 ```
+
+You get the picture.
+
+## Run
+
+`go run main.go`
+NOTE: Requires Go v1.20+ (has math/rand with seed auto-set).
+
+## Credits
+
+Bill Palmby of Prairie View, Illinois wrote the original version in BCG.
+
+## LICENSE
+Public domain, as was default for the original Palmby version.
+Additionally, David H . Ahl has explicitly released his work to the public.
+
+## Contributing
+
+This is probably done, but if you find a bug please file a PR or Issue.
+
+If you want to enhance, it is public domain, just fork it.
+
+## Possible Improvements
+
+* Two-player (note 3+ players requires actual deck modeling/card-counting)
+* Ruleset selection (Basic Computer Games, or Bicycle Cards)
+* Track wins-losses when the user opts to play again
+* Display probabilities/risks (and logic how this is calculated)
+* Emojis for cards
+* Support for [Bubble Tea](https://github.com/charmbracelet/bubbletea)
+* i18n
+* ..maybe use i18n to also toggle between 40 and 80-col, legacy UPPER case or modern Mixed like how some retro micros worked.
+* Display David Beker's card-playing robot in the Terminal [go-sixel](https://github.com/mattn/go-sixel) (with attribution and link of course)
+* A GUI [Ebitengine](https://ebitengine.org/)
+* Combine this with other card games into a single app, using subcommands to select games [Cobra](https://github.com/spf13/cobra)
+* Write as an HTTP API game server (let someone else write a client for it)
+* Use [goRemi, a prototype for card-playing game](https://github.com/ibrdrahim/goRemi)
+
+## References
+
+* [Acey Ducey, from BASIC Computer Games](https://www.atariarchives.org/basicgames/showpage.php?page=2)
+* [Basic Computer Games 1978 version (PDF)](https://annarchive.com/files/Basic_Computer_Games_Microcomputer_Edition.pdf)
+* [Basic Computer Games 1975 version (PDF)](http://www.bitsavers.org/pdf/dec/_Books/101_BASIC_Computer_Games_Mar75.pdf)
+* [In-Between: How to Play (Bicycle Cards)](https://bicyclecards.com/how-to-play/in-between/)
+* [In-Between: Game Rules](https://playingcarddecks.com/blogs/how-to-play/in-between-game-rules)
+* [Updating The Single Most Influential Book of the BASIC Era](https://blog.codinghorror.com/updating-the-single-most-influential-book-of-the-basic-era/)
+* [coding-horror/basic-computer-games](https://github.com/coding-horror/basic-computer-games)
+* [Red dog (card game)](https://en.wikipedia.org/wiki/Red_dog_(card_game)) (Slightly different game, but shares wiki page with Red Dog)
+* [Troy Campbell wrote the Go version of Acey Ducey in coding-horror](https://github.com/coding-horror/basic-computer-games/commits/main/00_Alternate_Languages/01_Acey_Ducey)
+* [David H. Ahl, PUBLIC DOMAIN](https://twitter.com/search?q=David%20ahl&src=typed_query&f=live)
+* [10 PRINT"LET'S READ BASIC COMPUTER GAMES":GOTO 10](https://forums.somethingawful.com/showthread.php?threadid=3928712)
+* [David H Ahl](https://en.wikipedia.org/wiki/David_H._Ahl)
+* [David Beker's Bots](http://www.bekerbots.com/)
+
+## Special Mentions
+
+Thanks to the author of BGC, [David H Ahl](https://en.wikipedia.org/wiki/David_H._Ahl) for collecting and documenting
+this amazing tome of simple (some not so simple) terminal games.
+
+Thanks also to the book's illustrator, [David Beker](http://www.bekerbots.com/), whose robots attracted the eye
+of many an eleven year old. BCG was not intended to be a kid's book, but thanks to the robots, it was.
---
+++ main.go
@@ -0,0 +1,207 @@
+// A Go implementation of "Acey Ducey" (In-Between) card game.
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Game rules match original BCG listing. See README.md for limitations.
+
+// trailing whitespace is intentionally added to about, heading.
+var bookIntro = `
+=============================================================================
+| THIS IS A SIMULATION OF THE ACEY DUCEY CARD GAME.                         |
+| IN THE GAME, THE DEALER (THE COMPUTER) DEALS TWO CARDS FACE UP.  YOU      |
+| HAVE AN OPTION TO BET OR NOT TO BET DEPENDING ON WHETHER OR NOT YOU       |
+| FEEL THE NEXT CARD DEALT WILL HAVE A VALUE BETWEEN THE FIRST TWO.         |
+|                                                                           |
+| YOUR INITIAL MONEY (BANKROLL) IS SET TO $100.  YOU MAY ALTER THIS VAR IF  |
+| YOU WANT TO START WITH MORE OR LESS THAN $100.  THE GAME KEEPS GOING      |
+| UNTIL YOU LOSE ALL YOUR MONEY OR INTERRUPT THE PROGRAM.                   |
+|                                                                           |
+| THE ORIGINAL PROGRAM AUTHOR WAS BILL PALMBY OF PRAIRIE VIEW, ILLINOIS.    |
+=============================================================================
+
+`
+
+var heading = `
+ACEY DUCEY CARD GAME
+CREATIVE COMPUTING  MORRISTOWN, NEW JERSEY
+
+
+`
+
+var gameIntro = `
+ACEY-DUCEY IS PLAYED IN THE FOLLOWING MANNER
+THE DEALER (COMPUTER) DEALS TWO CARDS FACE UP
+YOU HAVE AN OPTION TO BET OR NOT BET DEPENDING
+ON WHETHER OR NOT YOU FEEL THE CARD WILL HAVE
+A VALUE BETWEEN THE FIRST TWO.
+
+IF YOU DO NOT WANT TO BET, INPUT A 0`
+
+// var bankroll = 100
+var currency = "DOLLARS"
+
+// getCard returns an int of values 2 to 14, both inclusive. This is a flawed
+// deal in that it does not model/track the deck. But is OK for a single player.
+func getCard() int {
+	return rand.Intn(13) + 2
+}
+
+// getDealerCards returns a 2 element array, with each value being 2-14 inclusive,
+// and where elements are sorted increasing.  This follows the rules of the 1978
+// edition of BCG (NOTE: not exactly official rules).
+func getDealerCards() [2]int {
+	//
+	var dc [2]int
+	dc[0] = getCard()
+	// Prevent card A from exact matching card B (BCG rule)
+	for {
+		dc[1] = getCard()
+		if dc[1] != dc[0] {
+			break
+		}
+	}
+	// sort low-high
+	if dc[0] > dc[1] {
+		dc[0], dc[1] = dc[1], dc[0]
+	}
+
+	return dc
+}
+
+func getCardName(c int) string {
+	switch c {
+	case 11:
+		return "JACK"
+	case 12:
+		return "QUEEN"
+	case 13:
+		return "KING"
+	case 14:
+		return "ACE"
+	default:
+		return strconv.Itoa(c)
+	}
+}
+
+func end() {
+	fmt.Printf("\nOK HOPE YOU HAD FUN\n")
+	os.Exit(0)
+}
+
+func prompt() bool {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("SHOW BOOK TEXT? (Y or YES): ")
+		// ReadString will block until the delimiter arg is entered
+		response, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("AN ERROR OCCURED WHILE READING INPUT. PLEASE TRY AGAIN", err)
+		}
+
+		// remove delimeter
+		response = strings.ToLower(strings.TrimSuffix(response, "\n"))
+
+		if response == "y" || response == "yes" {
+			return true
+		} else if response == "n" || response == "no" || response == "" {
+			return false
+		}
+	}
+}
+
+// getBet returns an int representing a valid bet amount >= 0, after validation loop.
+func getBet(bankroll int) int {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Printf("\nWHAT IS YOUR BET? ")
+		response, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("AN ERROR OCCURED WHILE READING INPUT. PLEASE TRY AGAIN", err)
+		}
+
+		// remove delimeter
+		response = strings.ToLower(strings.TrimSuffix(response, "\n"))
+
+		// Handle "0" while string, first, since Atoi() will treat non-numeric characters as 0.
+		// Because 0 is valid, the "Chicken" logic is handled outside this loop.
+		if response == "0" {
+			return 0
+		}
+
+		// Cast input to string, and trap input errors
+		bet, err := strconv.Atoi(response)
+		if err != nil {
+			fmt.Println("AN ERROR OCCURED WHILE PARSING INPUT. PLEASE TRY AGAIN")
+			continue
+		}
+
+		// Trap negative value bets
+		if bet < 0 {
+			fmt.Println("PLEASE ENTER A POSITIVE NUMBER")
+			continue
+		}
+
+		// Trap too large value bets
+		if bet > bankroll {
+			fmt.Println("SORRY, MY FRIEND BUT YOU BET TOO MUCH")
+			fmt.Printf("YOU HAVE ONLY %d %s TO BET\n", bankroll, currency)
+			continue
+		}
+
+		// By this point we checked for all non-normal clauses)
+		//if bet > 0 && bet <= bankroll {
+		if bet > 0 {
+			// The bet is a valid amount not exceeding our wad.
+			return bet
+		}
+	}
+}
+
+func main() {
+	var bankroll = 100
+	fmt.Println(heading)
+	if prompt() {
+		fmt.Println(bookIntro)
+	}
+	fmt.Println(gameIntro)
+
+	for {
+		var dealerCards [2]int = getDealerCards()
+
+		fmt.Printf("YOU NOW HAVE %d %s.\n\n", bankroll, currency)
+		fmt.Printf("HERE ARE YOUR NEXT TWO CARDS:\n %s\n %s\n", getCardName(dealerCards[0]), getCardName(dealerCards[1]))
+
+		bet := getBet(bankroll)
+		//fmt.Println("*** DEBUG Your bet =", bet)
+
+		// 0 is a valid bet, but we end the current round.
+		if bet == 0 {
+			fmt.Printf("CHICKEN!!\n\n")
+			continue
+		}
+
+		playerCard := getCard()
+		fmt.Printf(" %s\n", getCardName(playerCard))
+
+		if playerCard > dealerCards[0] && playerCard <= dealerCards[1] {
+			fmt.Println("YOU WIN!!!")
+			bankroll = bankroll + bet
+		} else {
+			fmt.Println("SORRY, YOU LOSE")
+			bankroll = bankroll - bet
+		}
+
+		if bankroll <= 0 {
+			fmt.Println("SORRY, FRIEND, BUT YOU BLEW YOUR WAD")
+			end()
+		}
+	}
+}
